9b0bbcd6718cd7e867d785e107e677e2c5f83fa4
IasminiVoltoliniCorso <86911103+IasminiVoltoliniCorso@users.noreply.github.com>
2021-08-31 18:15:23 +0100

    Commit inicial

--- desafios/desafio026.py
+++ desafios/desafio026.py
@@ -2,5 +2,7 @@ f = str(input('Digite uma frase:')).strip().lower()
 print('A letra "a" aparece {} vezes.'.format(f.count('a')))
 print('A primeira vez que a letra "a" aparece é na posição {}.'.format(f.find('a') + 1))
 print('A última vez que a letra "a" aparace é na posição {}.'.format(f.rfind('a') + 1))
+print('Quantidade de carateres total: {} '.format(len(f)))
+
 
 
